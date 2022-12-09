56c5b242625de91690fcd040b1d7c80a93c05838
DXBoom <78609208+DXBoom@users.noreply.github.com>
2022-12-09 17:50:05 +0100

    Create CameraScript.lua

---
+++ CameraScript.lua
@@ -0,0 +1,20 @@
+local player = game.Players.LocalPlayer
+local camera = workspace.CurrentCamera
+
+player.CharacterAdded:Wait()
+player.Character:WaitForChild("HumanoidRootPart")
+
+camera.CameraSubject = player.Character.HumanoidRootPart
+camera.CameraType = Enum.CameraType.Attach
+camera.FieldOfView = 10
+
+local RunService = game:GetService("RunService")
+
+
+local function onUpdate()
+	if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+		camera.CFrame = CFrame.new(player.Character.HumanoidRootPart.Position) * CFrame.new(0, 0, 250)
+	end
+end
+
+RunService:BindToRenderStep("Camera", Enum.RenderPriority.Camera.Value, onUpdate)
